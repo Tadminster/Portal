41f2238e591e548cd7fe513993daa1a3dc717374
Tadminster <91bottlenine@naver.com>
2023-09-10 21:38:55 +0900

    8x8, 16x16 크기의 Feature 생성 가능하게 수정

--- Game1/Feature.cpp
+++ Game1/Feature.cpp
@@ -20,7 +20,7 @@ Feature::Feature(int type, int size)
 
 		// size 예외처리
 		if (size < 1) size = 1;
-		else if (size > 5) size = 5;
+		else if (size > 7) size = 7;
 
 		// size에 따라 texture 설정, 사이즈	설정
 		path = "concrete/";
@@ -31,6 +31,8 @@ Feature::Feature(int type, int size)
 			case 3: file = "concrete_modular_2x2.png"; tileSize = Vector3(6, 1, 6); break;
 			case 4: file = "concrete_modular_4x1.png"; tileSize = Vector3(12, 1, 12); break;
 			case 5: file = "concrete_modular_4x4.png"; tileSize = Vector3(12, 1, 12); break;
+			case 6: file = "concrete_modular_8x8.png"; tileSize = Vector3(24, 1, 24); break;
+			case 7: file = "concrete_modular_16x16.png"; tileSize = Vector3(48, 1, 48); break;
 		}
 	}
 	else if (type == 2)
@@ -40,7 +42,7 @@ Feature::Feature(int type, int size)
 
 		// size 예외처리
 		if (size < 1) size = 1;
-		else if (size > 3) size = 3;
+		else if (size > 5) size = 5;
 
 		// size에 따라 texture 설정
 		path = "metal/";
@@ -49,6 +51,8 @@ Feature::Feature(int type, int size)
 			case 1: file = "metal_modular_1x1.png"; tileSize = Vector3(3, 1, 3); break;
 			case 2: file = "metal_modular_2x2.png"; tileSize = Vector3(6, 1, 6); break;
 			case 3: file = "metal_modular_4x4.png"; tileSize = Vector3(12, 1, 12); break;
+			case 4: file = "metal_modular_8x8.png"; tileSize = Vector3(24, 1, 24); break;
+			case 5: file = "metal_modular_16x16.png"; tileSize = Vector3(48, 1, 48); break;
 		}
 	}
 	
